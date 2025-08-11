efa2414b5f48bd4cbcd7528f9d503b3d004e1ddb
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-11 10:57:11 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Bio from './views/Bio.vue'
 import Photos from './views/Photos.vue'
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(), 
+  history: createWebHistory(), 
   routes
 })
 
