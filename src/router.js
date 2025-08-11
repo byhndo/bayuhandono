6963fe978e5475eb76aaa1ea5fb9feff59d9dff2
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-11 11:35:25 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Bio from './views/Bio.vue'
 import Photos from './views/Photos.vue'
+import NotFound from './views/NotFound.vue' 
 
 const routes = [
   { path: '/', redirect: '/bio' },
   { path: '/bio', component: Bio },
   { path: '/photos', component: Photos },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
 
 const router = createRouter({
