43247e09917327a2547a5c98e864d64c14169f08
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-11 10:18:19 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path' 
+
+export default defineConfig({
+  base: '/handono/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') 
+    }
+  },
+  build: {
+    sourcemap: true, 
+  },
+  define: {
+    'window.jQuery': 'jQuery',
+    'window.$': 'jQuery',
+  }
+})
