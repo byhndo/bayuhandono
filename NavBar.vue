9afa861aea5bad1302b06b2fbf87ed7eac77e3a3
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-11 10:37:07 +0700

    Add files via upload

---
+++ NavBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from 'vue';
+
+const props = defineProps({
+  goToBio: Function,
+  goToPhotos: Function,
+  btnNav1: Object,
+  btnNav2: Object
+});
+
+onMounted(() => {
+  props.btnNav1.value = document.querySelector('.btn-about');
+  props.btnNav2.value = document.querySelector('.btn-photos');
+});
+</script>
+
+<template>
+  
+<div class="wrapnav">
+<div class="nav theme">
+<button @click="goToBio" ref="btnNav1" class="particles-button">About</button> 
+</div>
+<div class="nav theme">
+<button @click="goToPhotos" ref="btnNav2" class="particles-button">Photo</button> 
+</div>
+</div> 
+
+</template>
+
