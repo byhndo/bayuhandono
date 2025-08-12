42a31fb56ca3d3653a15cfaec925d456ccdf70ba
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-12 15:15:55 +0700

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,10 +21,10 @@ onMounted(() => {
   
 <div class="wrapnav">
 <div class="nav theme">
-<button @click="goToBio" ref="btnNav1" class="particles-button">About</button> 
+<button @click="goToBio" ref="btnNav1" class="particles-button btn-nav1">About</button> 
 </div>
 <div class="nav theme">
-<button @click="goToPhotos" ref="btnNav2" class="particles-button">Photo</button> 
+<button @click="goToPhotos" ref="btnNav2" class="particles-button btn-nav2">Photo</button> 
 </div>
 </div> 
 
