9f79da1a4dd05ef761c6011b5a9e8d5b4c05bdc3
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-13 16:49:56 +0700

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,43 @@
 <script setup>
-import {ref} from 'vue';
+import { ref, onMounted, watch } from 'vue';
+
+const btnNav1 = ref(null);
+const btnNav2 = ref(null);
+const props = defineProps({
+  activeRoute: String,
+  goToBio: Function,
+  goToPhotos: Function
+});
+
+const updateButtonColors = (path) => {
+  if (!btnNav1.value || !btnNav2.value) return;
+
+  let bgColor, textColor;
+
+  if (path === '/bio') {
+    bgColor = '#3C4048';   
+    textColor = '#e6e7eb'; 
+  } else if (path === '/photos') {
+    bgColor = '#e6e7eb';   
+    textColor = '#3C4048'; 
+} 
+
+  gsap.to([btnNav1.value, btnNav2.value], {
+    backgroundColor: bgColor,
+    color: textColor,
+    duration: .8,
+    delay: 1.03
+  });
+};
+
+ watch(() => props.activeRoute, (newPath) => {
+  updateButtonColors(newPath);
+}, { immediate: true });
+
+onMounted(() => {
+  updateButtonColors(props.activeRoute);
+});
+
 </script>
 
 
