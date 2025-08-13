08d7eb7030fad1076752abe8785f945623c1946d
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-13 13:37:17 +0700

    Create NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Halaman Tidak Ditemukan</h1>
+    <p>URL yang kamu masukkan tidak tersedia.</p>
+    <router-link to="/">Kembali ke Beranda</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
