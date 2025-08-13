f67f5ea8e8bbd8ac20447e7199681870c98ef24b
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-13 23:12:19 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path' 
-import fs from 'fs'
 
 export default defineConfig({
   base: '/',
-  plugins: [vue(),           
-     {
-      name: 'copy-index-to-404',
-      closeBundle() {
-        fs.copyFileSync('dist/index.html', 'dist/404.html')
-      }
-     }      
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src') 
