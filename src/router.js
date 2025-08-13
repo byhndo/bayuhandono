a7a3a72997e0f433cc1bc16379b1ee6e9adbeb3a
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-13 11:55:49 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import Bio from './views/Bio.vue'
 import Photos from './views/Photos.vue'
 
 const routes = [
-  { path: '/', redirect: '/bio' },
+  { path: '/', component: Bio },
   { path: '/bio', component: Bio },
   { path: '/photos', component: Photos }
 ]
