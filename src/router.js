8118098bca0432a1f9b322ee667084c93498c005
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-14 13:14:13 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,10 +5,10 @@ import Photos from './views/Photos.vue'
 import NotFound from './views/NotFound.vue'
 
 const routes = [
-  { path: '/', redirect: '/bio', meta: { showNav: true, showHeader: true } },
-  { path: '/bio', component: Bio, name: 'bio', meta: { showNav: true, showHeader: true } },
-  { path: '/photos', component: Photos, name: 'photos', meta: { showNav: true, showHeader: true } },
-  { path: '/:pathMatch(.*)*', component: NotFound, name: 'notfound', meta: { showNav: false, showHeader: false } }
+  { path: '/', redirect: '/bio' },
+  { path: '/bio', component: Bio, name: 'bio', meta: { hideNav: false } },
+  { path: '/photos', component: Photos, name: 'photos', meta: { hideNav: false } },
+  { path: '/:pathMatch(.*)*', component: NotFound, name: 'NotFound', meta: { hideNav: true } }
 ];
 
 const router = createRouter({
