da42ec79a77b2c0c648aadc101be11e711f1999a
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-14 13:40:54 +0700

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,30 +1,70 @@
 <template>
-<div class="container">
-<div class="main theme-404">	
-<svg class="hidden">		
-<defs>
-<filter id='noise' x='0%' y='0%' width='100%' height='100%'>
-<feGaussianBlur in="SourceGraphic" stdDeviation="0" result="blur"></feGaussianBlur>
-<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 14 -1" result="goo"></feColorMatrix>
-<feTurbulence type="fractalNoise" baseFrequency="0.009 1" numOctaves="1" seed="1" result="noise"></feTurbulence>
-<feDisplacementMap in="goo" in2="noise" scale="0" result="displacement"></feDisplacementMap>
-<feComposite in="SourceGraphic" in2="displacement" operator="atop"></feComposite>
-</filter>
-</defs>
-</svg>				  
-<div class="box">404</div>	
-<button id="btn404" class="particles-button">Home</button>
-</div>
-</div>
+  <div class="container">
+    <div class="main theme-404">
+      <svg class="hidden">
+        <defs>
+          <filter id="noise" x="0%" y="0%" width="100%" height="100%">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="0" result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 14 -1"
+              result="goo"
+            />
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.009 1"
+              numOctaves="1"
+              seed="1"
+              result="noise"
+            />
+            <feDisplacementMap
+              in="goo"
+              in2="noise"
+              scale="0"
+              result="displacement"
+            />
+            <feComposite
+              in="SourceGraphic"
+              in2="displacement"
+              operator="atop"
+            />
+          </filter>
+        </defs>
+      </svg>
+      <div class="box">404</div>
+      <button id="btn404" class="particles-button" @click="goHome">Home</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { inject, onMounted, onBeforeUnmount } from 'vue'
+import { useRouter } from 'vue-router'
 import Particles from '../utils/particles.js'
-import gsap  from 'gsap'
+import gsap from 'gsap'
 
-sessionStorage.redirect = location.pathname + location.search + location.hash;	
+const router = useRouter()
 
+// Ambil state global dari App.vue
+const showNav = inject('showNav')
+const showHeader = inject('showHeader')
+
+onMounted(() => {
+  // Sembunyikan nav & header
+  showNav.value = false
+  showHeader.value = false
+})
+
+onBeforeUnmount(() => {
+  // Tampilkan lagi saat keluar halaman
+  showNav.value = true
+  showHeader.value = true
+})
+
+function goHome() {
+  router.push('/')
+}
 </script>
 
 <style scoped>
