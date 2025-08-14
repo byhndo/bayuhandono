0d62a7a4c5a8b5e1c81ec676cdad13c906c15f0f
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-14 11:48:21 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,10 +5,10 @@ import Photos from './views/Photos.vue'
 import NotFound from './views/NotFound.vue'
 
 const routes = [
-{ path: '/', component: Bio },
-{ path: '/bio', component: Bio, name: 'bio' }, 
-{ path: '/photos', component: Photos, name: 'photos' },
-{ path: '/:pathMatch(.*)*', component: NotFound }
+  { path: '/', redirect: '/bio' },
+  { path: '/bio', component: Bio, name: 'bio' },
+  { path: '/photos', component: Photos, name: 'photos' },
+  { path: '/:pathMatch(.*)*', component: NotFound }
 ];
 
 const router = createRouter({
