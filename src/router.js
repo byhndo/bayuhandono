f02fe50469472a0a53949bb45af530f277dd1a4b
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-14 11:02:32 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,9 +5,10 @@ import Photos from './views/Photos.vue'
 import NotFound from './views/NotFound.vue'
 
 const routes = [
-  { path: '/', component: Bio, alias: '/bio', name: 'bio' }, // / dan /bio menampilkan Bio
-  { path: '/photos', component: Photos, name: 'photos' },
-  { path: '/:pathMatch(.*)*', component: NotFound, name: 'NotFound' } // catch-all
+{ path: '/', component: Bio, name: 'bio' },
+{ path: '/bio', component: Bio, name: 'bio-alias' }, 
+{ path: '/photos', component: Photos, name: 'photos' },
+{ path: '/:pathMatch(.*)*', component: NotFound }
 ];
 
 const router = createRouter({
