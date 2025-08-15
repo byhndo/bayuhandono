2d016d23c286632980b5b064e28b4968aa6c9e22
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-15 14:17:54 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Home from './views/Home.vue'
 import Bio from './views/Bio.vue'
 import Photos from './views/Photos.vue'
 import NotFound from './views/NotFound.vue'
 
 const routes = [
-  { path: '/bio', component: Bio, name: 'bio', alias: '/' },
+  { path: '/', component: Home, name: 'home' },
+  { path: '/bio', component: Bio, name: 'bio' },
   { path: '/photos', component: Photos, name: 'photos' },
   { path: '/:pathMatch(.*)*', component: NotFound, name: 'notfound' }
 ];
