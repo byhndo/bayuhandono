f0d97a68d81fd58c27dd1b407eae64a2b1293ef0
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-15 13:56:20 +0700

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -37,6 +37,122 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import '@/assets/normalize.css';
+import '@/assets/particles.css';
+import '@/style.css'; 
+	
+import { useRouter } from 'vue-router'
+const router = useRouter()
+import { onMounted } from 'vue'
+	
+import gsap from 'gsap';
+import Particles from '@/utils/particles.js'; 
+
+function goHome() {
+setTimeout(() => {
+    router.push('/bio')  
+  }, 2500)
+}
+	
+onMounted(() => {
+	
+const feBlur = document.querySelector(`#noise feGaussianBlur`);
+const feDisplacementMap = document.querySelector(`#noise feDisplacementMap`);
+
+let primitiveValues = { stdDeviation: 0, scale: 0 };
+ 
+const tl = gsap.timeline({
+    defaults: {
+      duration: 2,
+      ease: 'expo.out',
+},
+  onUpdate: function () {
+      feBlur.setAttribute('stdDeviation', primitiveValues.stdDeviation);
+      feDisplacementMap.setAttribute('scale', primitiveValues.scale); 
+    }
+  });
+
+tl.to(primitiveValues, { 
+    startAt: { stdDeviation: 40, scale: 100 },  
+    stdDeviation: 0,  
+    scale: 0 
+  }, 0)
+
+.to(".box", {
+    startAt: {
+      autoAlpha: 0,
+      opacity: 0,  
+      scale: .6     
+    },
+      autoAlpha:1,
+      opacity: 1,  
+      scale: 1 
+  }, 0)
+
+
+(function () {
+  const arrOpts = [    
+    {
+      direction: 'bottom',
+      duration: 1000,
+      easing: 'expo.in'
+    }
+  ];
+
+  const items = document.querySelectorAll(".main");
+
+  items.forEach((el, pos) => {
+    let bttn = el.querySelector("button.particles-button");
+
+    if (!bttn) return; 
+
+    let particlesOpts = arrOpts[pos];
+    const particles = new Particles(bttn, particlesOpts);
+
+    let tl = gsap.timeline();
+    tl.to(bttn, {
+      autoAlpha: 0,
+      onComplete: () => {
+        particles.integrate({
+          duration: 900,
+          easing: "easeOutSine"
+        });
+
+        gsap.to(bttn, {
+          duration: 1,	  
+          onComplete: () => {
+            bttn.style.opacity = "1";
+            bttn.style.visibility = "visible";
+	    bttn.style.pointerEvents = "none"; 
+	    gsap.to(bttn, {
+              onComplete: () => {
+                bttn.style.pointerEvents = "none"; 
+                gsap.to(bttn, {
+                  onComplete: () => {
+                  bttn.style.pointerEvents = "auto"; 
+                }
+              });
+             }
+            }); 
+          }
+        })
+      }
+    })
+
+
+      bttn.addEventListener("click", () => {
+      particles.disintegrate();
+    });
+  });
+})();
+
+	
+}); 
+
+</script>
+
 <style scoped>
 html, body {
  background: var(--bgloaderscreen);
@@ -119,118 +235,3 @@ a:hover, a:active{
 }
 }	
 </style>
-
-<script setup>
-import '@/assets/normalize.css';
-import '@/assets/particles.css';
-import '@/style.css'; 
-	
-import { useRouter } from 'vue-router'
-const router = useRouter()
-import { onMounted } from 'vue'
-	
-import gsap from 'gsap';
-import Particles from '@/utils/particles.js'; 
-
-function goHome() {
-setTimeout(() => {
-    router.push('/bio')  
-  }, 2500)
-}
-	
-onMounted(() => {
-	
-const feBlur = document.querySelector(`#noise feGaussianBlur`);
-const feDisplacementMap = document.querySelector(`#noise feDisplacementMap`);
-
-let primitiveValues = { stdDeviation: 0, scale: 0 };
- 
-const tl = gsap.timeline({
-    defaults: {
-      duration: 2,
-      ease: 'expo.out',
-},
-  onUpdate: function () {
-      feBlur.setAttribute('stdDeviation', primitiveValues.stdDeviation);
-      feDisplacementMap.setAttribute('scale', primitiveValues.scale); 
-    }
-  });
-
-tl.to(primitiveValues, { 
-    startAt: { stdDeviation: 40, scale: 100 },  
-    stdDeviation: 0,  
-    scale: 0 
-  }, 0)
-
-.to(".box", {
-    startAt: {
-      autoAlpha: 0,
-      opacity: 0,  
-      scale: .6     
-    },
-      autoAlpha:1,
-      opacity: 1,  
-      scale: 1 
-  }, 0)
-
-
-(function () {
-  const arrOpts = [    
-    {
-      direction: 'bottom',
-      duration: 1000,
-      easing: 'expo.in'
-    }
-  ];
-
-  const items = document.querySelectorAll(".main");
-
-  items.forEach((el, pos) => {
-    let bttn = el.querySelector("button.particles-button");
-
-    if (!bttn) return; 
-
-    let particlesOpts = arrOpts[pos];
-    const particles = new Particles(bttn, particlesOpts);
-
-    let tl = gsap.timeline()
-    tl.to(bttn, {
-      autoAlpha: 0,
-      onComplete: () => {
-        particles.integrate({
-          duration: 900,
-          easing: "easeOutSine"
-        });
-
-        gsap.to(bttn, {
-          duration: 1,	  
-          onComplete: () => {
-            bttn.style.opacity = "1";
-            bttn.style.visibility = "visible";
-	    bttn.style.pointerEvents = "none"; 
-	    gsap.to(bttn, {
-              onComplete: () => {
-                bttn.style.pointerEvents = "none"; 
-                gsap.to(bttn, {
-                  onComplete: () => {
-                  bttn.style.pointerEvents = "auto"; 
-                }
-              });
-             }
-            }); 
-          }
-        })
-      }
-    }, ">1.5")
-
-
-      bttn.addEventListener("click", () => {
-      particles.disintegrate();
-    });
-  });
-})();
-
-	
-}); 
-
-</script>
