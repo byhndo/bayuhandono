58bf0175533a7e914177c09b59ea8b4f0752e999
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-15 15:41:29 +0700

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,47 +1,21 @@
 <template>
-  <div class="container">
-    <div class="main theme-404" ref="root404">
-      <svg class="hidden">
-        <defs>
-          <filter id="noise404" x="0%" y="0%" width="100%" height="100%">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="0" result="blur" />
-            <feColorMatrix
-              in="blur"
-              mode="matrix"
-              values="1 0 0 0 0
-                      0 1 0 0 0
-                      0 0 1 0 0
-                      0 0 0 14 -1"
-              result="goo"
-            />
-            <feTurbulence
-              type="fractalNoise"
-              baseFrequency="0.009 1"
-              numOctaves="1"
-              seed="1"
-              result="noise"
-            />
-            <feDisplacementMap
-              in="goo"
-              in2="noise"
-              scale="0"
-              result="displacement"
-            />
-            <feComposite
-              in="SourceGraphic"
-              in2="displacement"
-              operator="atop"
-            />
-          </filter>
-        </defs>
-      </svg>
-
-      <div class="box">404</div>
-      <button id="btn404" class="particles-button" @click="goHome">
-        Home
-      </button>
-    </div>
-  </div>
+<div class="container">
+<div class="main theme-404">	
+<svg class="hidden">		
+<defs>
+<filter id='noise404' x='0%' y='0%' width='100%' height='100%'>
+<feGaussianBlur in="SourceGraphic" stdDeviation="0" result="blur"></feGaussianBlur>
+<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 14 -1" result="goo"></feColorMatrix>
+<feTurbulence type="fractalNoise" baseFrequency="0.009 1" numOctaves="1" seed="1" result="noise"></feTurbulence>
+<feDisplacementMap in="goo" in2="noise" scale="0" result="displacement"></feDisplacementMap>
+<feComposite in="SourceGraphic" in2="displacement" operator="atop"></feComposite>
+</filter>
+</defs>
+</svg>				  
+<div class="box">404</div>	
+<button id="btn404" class="particles-button" click="goHome">Home</button>
+</div>
+</div>
 </template>
 
 <script setup>
